922720036a21da2e8c26063a5bf09ff16a98ad37
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2024-11-19 16:14:06 -0600

    Convert to UTM16N

--- 01_setting_up_gps_data.R
+++ 01_setting_up_gps_data.R
@@ -3,7 +3,7 @@ library(sf)
 
 ## Mississippi shapefile
 ms <- st_read("data/landscape_data/mississippi_ACEA.shp") 
-acea <- st_crs(ms)
+acea <- st_crs(ms) #crs=9822
 ms <- ms %>% st_transform(crs=4326)
 
 # Greater Starkville
@@ -32,7 +32,7 @@ deer <- bind_rows(deer1, deer2, deer3)
 deer <- st_as_sf(deer, coords=c("Long", "Lat"), crs=4326) 
 
 deer <- st_intersection(deer, ms)
-deer <- deer%>% st_transform(crs=acea)
+deer <- deer %>% st_transform(crs=32616)
 
 #### Save shapefiles of each deer study ####
 
@@ -70,20 +70,6 @@ npts <- deer %>% group_by(DeerID) %>% count()
 hist(npts$n)
 sum(npts$n<1000)
 
-# un.id <- unique(deer$DeerID)
-# for (i in 1:length(un.id)) {
-#   
-#   temp <- deer %>% filter(DeerID==un.id[i])
-#   
-#   ggplot(temp) +
-#     geom_path(aes(x=X, y=Y, color=timestamp)) +
-#     theme_bw()
-#   
-#   ind <- un.id[i]
-#   ggsave(paste0("figs/deer_movement/deer",ind,"movement.png"))
-#   
-# }
-
 # Keep deer with >3 months of data
 deer_time <- deer %>% group_by(DeerID) %>%
                 reframe(ts=range(timestamp)) %>%
@@ -92,22 +78,35 @@ deer_time <- deer %>% group_by(DeerID) %>%
                 mutate(duration=end-start,
                        months=as.numeric(duration/30))
 
-deer_mean <- deer %>% group_by(id) %>% reframe(meanX=mean(X), meanY=mean(Y))
+keep <- deer_time %>% filter(months > 3) %>% select(DeerID) %>% as.vector()
+keep <- unlist(unname(keep))
 
-ms <- ms %>% st_transform(crs=acea)
+deer <- deer %>% filter(DeerID %in% keep)
 
-ggplot(deer_mean) +
-  geom_sf(data=ms) +
-  geom_point(aes(x=meanX, y=meanY, color=factor(id))) +
-  theme_bw() +
-  theme(legend.position="none")
+
+write_csv(deer, "data/location_data/deer_filtered.csv")
+
 
 
 #### Pigs ####
-pigs1 <- read_csv("data/location_data/Cleaned_DeltaMS_PigGPS_data.csv") %>% select(-Sex, -Temp) %>% rename(id=collar_id)
-pigs2 <- read_csv("data/location_data/Cleaned_NorthMS_PigGPS_data.csv") %>% select(-Altitude, -Temp) 
-pigs3 <- read_csv("data/location_data/Cleaned_Noxubee_PigGPS_data.csv") %>% select(-Temp) %>% rename(id=CollarID, datetime=DateTime)
-pigs5 <- read_csv("data/location_data/Cleaned_EasternMS_PigGPS_data.csv") %>% select(-Altitude, -Temp) %>% rename(id=collar_id)
+# Read GPS data
+pigs1 <- read_csv("data/location_data/Cleaned_DeltaMS_PigGPS_data.csv") %>% select(-Sex, -Temp) %>% rename(id=collar_id) %>% mutate(study="Delta")
+pigs2 <- read_csv("data/location_data/Cleaned_NorthMS_PigGPS_data.csv") %>% select(-Altitude, -Temp, -DOP) %>% mutate(study="Northern")
+pigs3 <- read_csv("data/location_data/Cleaned_Noxubee_PigGPS_data.csv") %>% select(-Temp) %>% rename(id=CollarID, datetime=DateTime, Long=x, Lat=y) %>% mutate(study="Noxubee")
+pigs4 <- read_csv("data/location_data/Cleaned_EasternMS_PigGPS_data.csv") %>% select(-Altitude, -Temp, -DOP) %>% rename(id=collar_id) %>% mutate(study="Eastern")
+
+# Combine
+pigs <- bind_rows(pigs1, pigs2, pigs3, pigs4)
+
+# Remove locations that are outside of mississippi
+pigs <- st_as_sf(pigs, coords=c("Long", "Lat"), crs=4326) 
+
+# st_write(pigs, "data/location_data/all_pig_points.shp")
+
+pigs <- st_intersection(pigs, ms)
+pigs <- pigs %>% st_transform(crs=acea)
+
+
 
 
 
