e471bff1b94c1b55f74b953638be77e6dc247cc3
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-01-07 07:55:50 -0600

    Combined data extraction and running RSF model into single script

--- 03_deer_HR_pct_landcover_RSF.R
+++ 03_deer_HR_pct_landcover_RSF.R
@@ -5,25 +5,18 @@ library(exactextractr)
 
 ## Read location data
 deer <- read_csv("data/location_data/deer_used_avail.csv")
-pigs <- read_csv("data/location_data/pigs_used_avail.csv")
 
 # convert to sf object
 deer <- st_as_sf(deer, coords=c("X", "Y"), crs=32616)
-pigs <- st_as_sf(pigs, coords=c("X", "Y"), crs=32616)
 
 # Reproject to ACEA
 deer <- st_transform(deer, crs=5070)
-pigs <- st_transform(pigs, crs=5070)
-
-# create buffers
-# deer_buff <- deer %>% st_buffer(100) %>% st_as_sf()
 
 ## Load rasters
 ms_full <- vect("data/landscape_data/mississippi_ACEA.shp") 
 ms_buff <- vect("data/landscape_data/mississippi_ACEA_50kmbuffer.shp")
 
 deer_buffer <- vect("data/landscape_data/deer_10km_buffers.shp")
-pigs_buffer <- vect("data/landscape_data/PigsGPS_10kmBuffer.shp")
 
 ## Load rasters
 rast_list <- c("data/landscape_data/2pt3km2_bottomlandSUM.tif",
@@ -64,6 +57,61 @@ deer <- deer %>% separate(DeerID, into=c("id", "study", "year"), sep="_") %>%
 
 # Save file
 write_csv(deer, "data/deer_usedavail_HRcovariates.csv")
+
+library(lme4)
+set.seed(1)
+
+# Run model
+m1 <- glmer(type ~ pctBottomland + pctDecid + pctEvergreen + pctWater + pctHerbaceous + pctCrops + (1|DeerID), data=deer, family=binomial(link = "logit"))
+
+## Create empty raster
+temp_rast <- rast(ext(ms_full), resolution=1000) # create template raster 1 km x 1 km
+
+# Predict
+pred_deer <- predict(layers, m1, type="response", re.form = NA)
+
+# Mask to Mississippi
+pred_deer <- mask(pred_deer, ms_full)
+
+# Resample to 1.5 x 1.5 km for RAMAS
+pred_deer <- resample(pred_deer, temp_rast)
+pred_deer <- mask(pred_deer, ms_full)
+pred_deer <- crop(pred_deer, ext(ms_full))
+plot(pred_deer)
+
+# Rescale to be between 0 and 1
+pred_deer <- (pred_deer-minmax(pred_deer)[1])/(minmax(pred_deer)[2]-minmax(pred_deer)[1])
+plot(pred_deer)
+
+# Reclassify missing data to 0
+m <- rbind(c(NA, 0))
+pred_deer <- classify(pred_deer, m)
+
+writeRaster(pred_deer, "data/predictions/deer_glmm_rsf_FINALFINALFINALFINAL.tif", overwrite=TRUE)
+writeRaster(pred_deer, "data/predictions/deer_glmm_rsf_FINALFINALFINALFINAL.asc",NAflag=-9999, overwrite=TRUE)
+
+
+all_vals <- as.data.frame(pred_deer)
+range(all_vals$lyr1)
+mean(all_vals$lyr1)
+quantile(all_vals$lyr1, probs=c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1))
+
+## Extract RSF predictions at each location
+deer_test <- deer %>% select(DeerID:Y)
+
+dat_deer <- extract(pred_deer, deer_test[,c(4:5)])
+
+deer_test$rsf <- dat_deer$lyr1
+
+hist(deer_test$rsf[deer_test$type==1], xlim=c(0,1))
+hist(deer_test$rsf[deer_test$type==0], xlim=c(0,1))
+
+min(deer_test$rsf[deer_test$type==1], na.rm=TRUE)
+mean(deer_test$rsf[deer_test$type==1], na.rm=TRUE)
+median(deer_test$rsf[deer_test$type==1], na.rm=TRUE)
+
+quantile(deer_test$rsf[deer_test$type==1], probs=c(0.25,0.5,0.75, 0.95))
+
 
 
 
