6476881e6270778d97376946687fcbdcc6b5a13f
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-03-10 08:32:22 -0500

    Initial commit

---
+++ 02_generating_30x30_points_by_county.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(terra)
+
+# Read in a 30x30 raster as a template
+crops <- rast("data/landscape_data/palatable_crops_180m_sum.tif")
+
+# Reclassify missing data to 0
+m <- rbind(c(NA, 0))
+crops <- classify(crops, m)
+
+# Convert to percent
+crops <- crops / 113
+
+# Read shapefile of MS counties and reproject
+counties <- vect("data/landscape_data/county_nrcs_a_ms.shp")
+counties <- project(counties, crops)
+
+# Create a list where each element is a county
+split_counties <- split(counties, "COUNTYNAME") 
+
+# Loop over each county and convert cells to points
+for (i in 1:length(split_counties)) {
+  
+  # Subset just a single county
+  cty <- split_counties[[i]]
+  
+  # Extract county name and create a filename
+  n <- unique(cty$COUNTYNAME)
+  filename <- paste0("data/landscape_data/county_points/", n, "_points.shp")
+  
+  # Buffer the county by 5 km
+  ctyB <- buffer(cty, width=5000)
+  
+  # Mask and crop raster to county
+  temp <- mask(crops, ctyB)
+  temp <- crop(temp, ctyB)
+  
+  # Convert to points
+  pts <- as.points(temp, values=FALSE, na.rm=TRUE, na.all=FALSE)
+  
+  # Write shapefile
+  writeVector(pts, filename, overwrite=TRUE)
+  
+}
+
+
+
+
+
---
+++ 02_pig_home_ranges.R
@@ -0,0 +1,284 @@
+library(tidyverse)
+library(amt)
+library(sf)
+library(adehabitatLT)
+library(ctmm)
+library(patchwork)
+
+## Set ggplot theme
+theme_set(theme_bw())
+
+## Write function to subset and convert to ltraj
+# Will need this for segmenting migration
+lv_func <- function(x, id_) {
+  
+  # Subset by ID
+  temp <- x %>% 
+    # Filter by ID
+    filter(id==id_) %>%
+    # Select only columns we need to converting back to ltraj
+    dplyr::select(id:date) %>%
+    # Convert to data.frame
+    as.data.frame() %>%
+    # Make date a POSIX-formatted column
+    mutate(date = as.POSIXct(date, tz="America/Denver"))
+  
+  # Convert to ltraj
+  ltr <- as.ltraj(xy=temp[,c("x","y")], date=temp$date, id=temp$id)
+  
+  # Run lavielle
+  lv <- lavielle(ltr, Lmin=30, which="R2n", Kmax=10, plotit=TRUE)
+  
+  k_try <- chooseseg(lv, S=0.75, output="opt", draw=TRUE)
+  
+  print("Optimal breaks by chooseseg():")
+  print(k_try)
+  
+  # Plot and segment with select value of breaks
+  fp <- findpath(lv, k_try)
+  
+  # Take input from the console on number of breaks
+  print("Enter number of breaks:")
+  k <- scan(n = 1)
+  
+  # Plot and segment with select value of breaks
+  fp <- findpath(lv, k)
+  
+  # Convert to data frame
+  df <- ld(fp)
+  # remove some columns we don't need (i.e. match pigs_res)
+  df <- df %>% as_tibble() %>%
+    # reorder columns
+    dplyr::select(id, x:rel.angle, burst) %>%
+    # Remove extra characters ("Segment.") so that we only have a number in the burst column
+    mutate(burst=gsub("Segment.", "", burst)) %>%
+    # Combine ID with the segment number
+    unite(key, c("id", "burst"), sep="_", remove=FALSE) %>%
+    # Reorder columns
+    dplyr::select(id, key, x:rel.angle)
+  
+  # Return segmented data
+  return(df)
+}
+
+## Create template raster for generating random points
+
+
+#### pigs ####
+# Read in pigs location data
+pigs <- read_csv("data/location_data/pigs_filtered.csv") %>%
+  mutate(month=as.numeric(format(timestamp, "%m")),
+         year=as.numeric(format(timestamp, "%Y"))) %>%
+  dplyr::select(PigID:study,month,year,timestamp,X,Y)# %>%
+  # filter(PigID != "35493_Noxubee_2022")
+
+pigs %>% group_by(study) %>% reframe(range=range(timestamp))
+
+# Create track of all individuals
+pigs_tr <- make_track(pigs, X, Y, timestamp, id=PigID, crs=32616)
+
+# Check sampling rate of all individuals
+trk_info <- pigs_tr %>% summarize_sampling_rate_many(c("id"), time_unit="min")
+
+# List individual IDs
+un.id <- unique(pigs$PigID)
+
+## Loop over each ID and: 
+# 1. Resample to every 4 hours
+# 2. Calculate step lengths (and find median)
+# 3. Check patchiness of location data
+# 4. Determine whether individuals are moving ranges throughout the year
+# 5. Build ctmm home range
+# 6. Extract available points from home range
+
+
+#### 1. and 2. Run loop to resample and calculate step lengths ####
+pigs_res <- data.frame()
+dist <- numeric()
+tr_sum <- data.frame()
+for (i in 1:length(un.id)) {
+  
+  # Filter to ID
+  dat <- pigs_tr %>% filter(id==un.id[i])
+  
+  # Filter all locations to ~4 hours
+  dat <- track_resample(dat, rate=hours(4), tolerance=minutes(5))
+  s <- dat %>% summarize_sampling_rate_many(c("id"), time_unit="hour")
+  tr_sum <- bind_rows(tr_sum, s)
+  
+  # Create column for only dates, count how many unique days
+  dat <- dat %>% mutate(date=as.Date(format(t_, "%Y-%m-%d"))) 
+  
+  # How many locations on each day?
+  nobs <- dat %>% group_by(date) %>% count() %>% filter(n>=5)
+  un.days <- unique(nobs$date)
+  
+  # Skip to next individual if less than 90 days of data
+  if (length(un.days) >= 90) {
+    
+    # Convert to ltraj
+    df <- as.data.frame(dat)
+    df <- as.ltraj(xy=df[,c("x_","y_")], date=df$t_, id=df$id)
+    
+    ## Regularize trajectories, round values
+    refda <- min(df[[1]]$date)
+    df_NA <- setNA(df, refda, 240, units="min") 
+    df <- sett0(df_NA, refda, 240, units="min")
+    
+    # Save distance between locations
+    dist <- c(dist, df[[1]]$dist)
+    
+    df <- ld(df)
+    df <- df %>% as_tibble() %>%
+      dplyr::select(id, x:rel.angle)
+    
+    # Save resampled data
+    pigs_res <- bind_rows(pigs_res, df)
+  } # if statement
+} # i
+
+tr_sum <- tr_sum %>% as_tibble()
+
+
+#### 3. Determine which animals need to be split ####
+pigs_res <- as_tibble(pigs_res)
+un.id <- unique(pigs_res$id)
+
+for (i in 1:length(un.id)) {
+  
+  temp <- pigs_res %>% filter(id==un.id[i])
+  
+  p1 <- ggplot(temp, aes(x=x, y=y, color=date)) +
+    geom_path() + geom_point() +
+    ggtitle(un.id[i])
+  
+  p2 <- ggplot(temp, aes(x=date, y=R2n, color=date)) +
+    geom_path()
+  
+  print(p1 / p2)
+  
+}
+## Look through plots and manually list which IDs probably will need to be segmented, as well as those that might need to be filtered by dates to remove patchy data
+
+to_segment <- c("19219_Delta", "152316_Northern", "152318_Northern", "26620_Noxubee", "35490_Noxubee") # "26626_Noxubee", "19223_Delta",
+
+# Subset data without pigs that need to be segmented (so that we can add the segments onto it)
+pigs <- pigs_res %>% 
+  # Filter out NAs
+  filter(!is.na(x)) %>%
+  # Remove IDs that will be segmented
+  filter(!(id %in% to_segment)) %>%
+  # Create a 'key' column to match the output from the Lavielle function
+  mutate(burst=1) %>%
+  unite("key", c("id", "burst"), sep="_", remove=FALSE) %>%
+  dplyr::select(id, key, x:rel.angle)
+
+## Need to do this manually, and make sure to add the segmented data to 'pigs'
+s <- lv_func(pigs_res, "35490_Noxubee")  
+
+# 19219_Delta : 3
+# 152316_Northern : 5
+# 152318_Northern : 5
+# 26620_Noxubee : 2
+# 35490_Noxubee : 2
+
+# Add segmented data to full dataset
+pigs <- bind_rows(pigs, s)
+
+# Check if there are any IDs in the to_segment vector that are not in the pigs tibble after segmenting and binding
+to_segment[!(to_segment %in% pigs$id)]
+
+
+# Count how many days in a segment, remove less than 1 month  [did not actually do]
+
+#### 4. Loop over individuals and calculate AKDE home range ####
+
+## Set up data
+# Convert to lat/long (WGS84)
+pigs_sf <- pigs %>% 
+  # Remove rows without coordinates
+  filter(!is.na(x)) %>% 
+  # Convert to sf object
+  st_as_sf(coords=c("x", "y"), crs=32616) %>%
+  # Reproject
+  st_transform(crs=4326)
+# save lat/lon
+ll <- pigs_sf %>% st_coordinates() %>% as_tibble() %>% 
+  rename(location.long=X, location.lat=Y)
+
+pigs <- pigs %>% 
+  filter(!is.na(x)) %>% 
+  bind_cols(ll)
+
+dat <- pigs %>%      
+  # need the lat long, etc. in a specific order 
+  dplyr::select(key, location.long, location.lat, date) %>% 
+  # also need columns to have a specific name
+  rename(individual.local.identifier=key, timestamp=date)
+
+# Create telemetry object
+dat <- as.telemetry(dat, timeformat="%Y-%m-%d %H:%M:%S", timezone=attr(pigs_res$timestamp, "tzone"), projection=NULL)
+
+## Loop over all individuals / individual segments
+ids <- unique(pigs$key)
+
+# Create list to save variograms and AKDEs
+out <- list()
+
+# Create empty object to store 
+size <- data.frame()
+
+# Set progress bar of sanity because this takes long - set it to the number of ids in the dataset (min = 0, max = max number of ids)
+pb <- txtProgressBar(min = 0, max = length(ids), style = 3)
+
+# Run loop to fit AKDEs
+for (i in 1:length(ids)) {
+  
+  try({ # Some will likely fail, so prevent the loop from crashing 
+    setTxtProgressBar(pb, i)
+    
+    # Next three lines are to fit variogram and select the best movement model to fit to the data
+    var <- variogram(dat[[i]]) 
+    var2 <- variogram.fit(var,  name="GuessBM", interactive=F)
+    # Automated model selection
+    tt <- ctmm.select(dat[[i]], var2) 
+    
+    # Fit AKDE
+    UD <- akde(dat[[i]], tt, weights=TRUE) 
+    
+    # Save area of AKDE
+    sqkm <- as.data.frame(summary(UD)$CI)
+    size <- bind_rows(size, sqkm)
+    
+    # Plot with points
+    plot(dat[[i]], UD)
+    
+    # Save the variograms and akde in a list  
+    out[[i]] <- list(dat[[i]]@info$identity, var, var2, UD) 
+  })
+}
+saveRDS(out, "results/raw_pigs_AKDEs.rds")
+
+# Take just the AKDE
+akde <- lapply(out, function(x) x[[4]]) 
+
+# Save the IDs
+ids <- lapply(out, function(x) x[[1]])
+ids <- unlist(ids)
+
+# add IDs to size df
+size$id <- ids
+
+
+# Convert UD to sf object (multipolygons)
+poly <- lapply(akde, as.sf, level.UD=0.99)
+
+# Reproject to UTM 16N
+poly <- lapply(poly, st_transform, crs=32616)
+
+
+ud <- do.call(rbind, poly)
+
+plot(st_geometry(ud))
+
+
