db8891b626d8ed0cf1a2e5d37e2b84d0ba3adb4f
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-03-17 08:48:03 -0500

    Initial commit

---
+++ 05_predicting_pig_map.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(terra)
+
+theme_set(theme_bw())
+
+#### Read Data ####
+# locations (used & available)
+pigs <- read_csv("output/pigs_used_avail_locations.csv") %>%
+  # Add column for weight
+  mutate(weight=if_else(case==1, 1, 5000))
+
+# Rasters
+rast_list <- c("data/landscape_data/shrublands_210m_sum.tif",
+               "data/landscape_data/othercrops_210m_sum.tif",
+               "data/landscape_data/gramanoids_210m_sum.tif", 
+               "data/landscape_data/barren_210m_sum.tif", 
+               "data/landscape_data/developed_210m_sum.tif",
+               "data/landscape_data/palatable_crops_210m_sum.tif") 
+layers <- rast(rast_list)
+
+# Reclassify missing data to 0
+m <- rbind(c(NA, 0))
+layers <- classify(layers, m)
+
+# Convert to % 
+layers <- layers / 149
+
+# Forest rasters (accounting for 50% mixed decid/evergreen)
+forest <- c("data/landscape_data/evergreenMODIFIED_210m_sum.tif",
+            "data/landscape_data/allhardwoodsMODIFIED_210m_sum.tif")
+forest <- rast(forest)
+
+# Reclassify missing data to 0
+forest <- classify(forest, m)
+
+# Convert to % 
+forest <- forest / 298
+
+layers <- c(layers, forest)
+
+# read water
+water <- rast("data/landscape_data/RSinterarealMerge_distance30m.tif")
+water <- resample(water, layers)
+ext(water) <- ext(layers)
+
+layers <- c(layers, water)
+
+# Center and scale continuous rasters
+layers <- scale(layers)
+
+# Rename layers
+names(layers) <- c("shrubs", "othercrops", "gramanoids", "barren", "developed", "foodcrops", "evergreen", "deciduous", "water")
+
+## first, subset to only layers that were in the model
+
+
+
