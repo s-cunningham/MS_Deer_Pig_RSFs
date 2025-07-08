30a9e6ded155d3b6ba78f66541dfe3188450e965
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-07-07 20:02:13 -0500

    Initial commit

---
+++ suitability_threshold_areas.R
@@ -0,0 +1,7 @@
+
+
+
+hab <- data.frame(species=rep(c("deer", "pigs"), each=3),
+                  type=rep(c("core","marginal","verymarginal"), 2),
+                  ncells=c(5436507,5409943,3665703,6346538,6340520,1906219),
+                  avgHS=c(0.975,0.927,0.743,0.702,0.619,0.515))
