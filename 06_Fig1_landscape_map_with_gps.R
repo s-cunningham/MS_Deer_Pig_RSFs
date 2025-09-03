b5f0888f754df44d16cb53ff0db4a0a502eb80bc
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-09-03 10:20:24 -0500

    Added code for landscape map, need to fix GPS shapefile

---
+++ 06_Fig1_landscape_map_with_gps.R
@@ -0,0 +1,102 @@
+library(tidyverse)
+library(terra)
+library(tidyterra)
+library(ggspatial)
+
+## Read GPS locations
+# Pigs
+pigs <- read_csv("data/location_data/pigs_filtered.csv")
+pigs <- vect(pigs, geom=c("X", "Y"), crs=32616)
+
+# Deer
+deer <- read_csv("data/location_data/deer_filtered.csv")
+
+## Create 1km buffers
+# Pigs
+pigs <- vect("data/location_data/pigs_1km_buffer.shp")
+
+# Deer
+deer <- vect("data/location_data/deer_1km_buffer.shp")
+
+## Read spatial data
+nlcd <- rast("data/landscape_data/NLCD2023_MS50kmbuff_90m.tif")
+ms <- vect("data/landscape_data/mississippi_ACEA.shp")
+
+## Create empty raster
+temp_rast <- rast(ext(ms), resolution=1000) # create template raster 1 km x 1 km
+
+# Mask to Mississippi
+nlcd <- mask(nlcd, ms)
+
+# Add color table for plotting
+FedData::nlcd_colors()
+coltab(nlcd) <- data.frame(value=c(11, 21, 22, 23, 24, 31, 41, 42, 43, 52, 71, 81, 82, 90, 95), 
+                           col=c("#5475A8","#E8D1D1","#E29E8C","#ff0000","#B50000",
+                                                   "#D2CDC0","#85C77E","#38814E","#D4E7B0","#DCCA8F",
+                                                   "#FDE9AA","#FBF65D","#CA9146","#C8E6F8","#64B3D5"))
+
+# Set up NLCD classes and class values
+nlcd_values <- c(11, 21, 22, 23, 24, 31, 41, 42, 43, 52, 71, 81, 82, 90, 95)
+nlcd_class <- c("Open Water", "Developed, Open Space", "Developed, Low Intensity", 
+                "Developed, Medium Intensity", "Developed, High Intensity", "Barren", 
+                "Deciduous Forest", "Evergreen Forest", "Mixed Forest", "Shrub/Scrub",
+                "Grassland/Herbaceous", "Pasture/Hay", "Cultivated Crops", "Woody Wetlands", 
+                "Emergent Herbaceous Wetlands")
+
+# Add class names and numbers to the raster
+levels(nlcd) <- list(data.frame(ID = nlcd_values,
+                                landcov = nlcd_class))
+
+# Resample to 1 x 1 km for plot
+# nlcd <- resample(nlcd, temp_rast, method="near")
+nlcd <- mask(nlcd, ms)
+nlcd <- crop(nlcd, ext(ms))
+
+# Reproject polygons
+deer <- project(deer, nlcd)
+pigs <- project(pigs, nlcd)
+
+
+map <- ggplot() +
+  geom_spatraster(data=nlcd) +
+  scale_fill_coltab(data=nlcd,   na.translate = FALSE,
+                    na.value = "transparent", name="NLCD") +
+  geom_spatvector(data=pigs, col="black", fill="black", alpha=0.9, linewidth=0.7) +
+  geom_spatvector(data=deer, col="black", fill="white", alpha=0.9, linewidth=0.7) +
+  annotation_north_arrow(
+    which_north = TRUE,
+    pad_x = unit(0.05, "npc"),
+    pad_y = unit(0.85, "npc"),
+    style = north_arrow_minimal()) +
+  annotation_scale(
+    height = unit(0.015, "npc"),
+    width_hint = 0.5,
+    pad_x = unit(0.07, "npc"),
+    pad_y = unit(0.07, "npc"),
+    text_cex = .8) +
+  theme_classic() +
+  theme(panel.border=element_rect(fill=NA, color="black"),
+        legend.margin=margin(t = 12, unit='cm')) 
+
+# Create inset
+us <- vect("data/landscape_data/north_am_states.shp")
+us <- project(us, deer)
+
+inset <- ggplot() +
+  ylim(500000,1800000) + xlim(-500000,1500000) +
+  geom_spatvector(data=us) +
+  geom_spatvector(data=ms, color="gray30", fill="gray30") +
+  theme_bw() +
+  theme(panel.border=element_rect(fill=NA, color="black"))
+
+library(patchwork)
+layout <- c(
+  area(t = 1, l = 1, b = 15, r = 4),  # map
+  area(t = 0, l = 3, b = 4, r = 4)  # inset?
+)
+
+map + inset +
+  plot_layout(design = layout)
+
+# ggsave("figs/figure1_map.svg")
+# Saving 8.47 x 9.06 in image
