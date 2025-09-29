8c968bb6a41274a5c6ad34ddd69653a620f4dde4
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2025-09-29 09:04:23 -0500

    Initial commit

---
+++ 09_plotting_pop_projections.R
@@ -0,0 +1,26 @@
+
+library(tidyverse)
+library(ggtext)
+library(patchwork)
+
+## Read results ####
+
+# Deer
+deer <- read_csv("results/deer_population_projections.csv")
+deer_s <- read_csv("results/deer_survival.csv")
+deer_f <- read_csv("results/deer_fecundity.csv")
+
+# Pigs
+pigs <- read_csv("results/pigs_population_projections.csv")
+pigs_s <- read_csv("results/pigs_survival.csv")
+pigs_f <- read_csv("results/pigs_fecundity.csv")
+
+## Plot ####
+
+## Population projection
+
+## Survival
+
+
+## Fecundity
+
