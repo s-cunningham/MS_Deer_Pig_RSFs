9a11c4252b318e909d9ec45377de11975c878966
Stephanie Cunningham <39537658+s-cunningham@users.noreply.github.com>
2026-07-09 21:24:28 -0500

    Added deer rates from literature

--- 09_plotting_pop_projections.R
+++ 09_plotting_pop_projections.R
@@ -3,11 +3,43 @@ library(tidyverse)
 library(ggtext)
 library(patchwork)
 
+## Read demographic rates from literature ####
+rates <- read_csv("data/literature_demographic_rates.csv") |>
+  mutate(sex = ifelse(sex=="female", "Female", "Male"))
+
+rates$age <- factor(rates$age, 
+                    levels=c("fawn","yearling","2-year-old","3-year-old","4-year-old","5+ years-old"), 
+                    labels=c("Fawn","Yearling","2-year-old","3-year-old","4-year-old","5+ years-old"))
+
+rates <- rates |>
+  rename(stage = age) |>
+  mutate(stage = as.character(stage)) |>
+  mutate(x=case_when(stage=="Fawn" ~ 0.2,
+                     stage=="Yearling" ~ 0.3,
+                     stage=="2-year-old" ~ 0.4,
+                     stage=="3-year-old" ~ 0.5,
+                     stage=="4-year-old" ~ 0.6,
+                     stage=="5+ years-old" ~ 0.7)) |>
+  mutate(x = if_else(sex=="Female", x - 0.01, x + 0.01))
+
+
+ggplot(rates) +
+  coord_cartesian(ylim=c(0,1)) +
+  geom_point(aes(x=age, y=survival), color="gray", alpha=0.5, size=3) +
+  # scale_shape_manual(values=c(21,22,23,24,25,0:18)) +
+  facet_wrap(vars(sex), ncol=2) +
+  theme(legend.position="none",
+        panel.grid=element_blank()) 
+  
+
+
+
 ## Read results ####
 
 # Deer
 deer <- read_csv("results/deer_population_projections.csv")
-deer_s <- read_csv("results/deer_survival.csv") 
+deer_s <- read_csv("results/deer_survival.csv") |>
+  filter(source=="Implied")
 deer_f <- read_csv("results/deer_fecundity.csv")
 
 # Pigs
@@ -21,7 +53,7 @@ pigs_f <- read_csv("results/pig_fecundity.csv")
 ## Population projection
 deer_pop <- ggplot(deer) +
   coord_cartesian(ylim=c(0,3000000)) +
-  # geom_hline(yintercept=1610000, color="gray", linewidth=1) +
+  geom_hline(yintercept=1610000, color="gray", linewidth=1) +
   geom_hline(yintercept=1347232, linetype=2, color="#ed7953", linewidth=1) +
   geom_hline(yintercept=2090532, linetype=2, color="#9c179e", linewidth=1) +
   geom_ribbon(aes(x=Year, ymin=N.10pct, ymax=N.90pct, fill=density, group=density), alpha=.2) +
@@ -54,36 +86,66 @@ deer_s <- deer_s |>
                      stage=="3-year-old" ~ 0.5,
                      stage=="4-year-old" ~ 0.6,
                      stage=="5+ years-old" ~ 0.7)) |>
-  mutate(x = if_else(sex=="Female", x - 0.01, x + 0.01))
+  mutate(x = if_else(sex=="Female", x - 0.01, x + 0.01)) #|>
+  # filter(density=="14.3 deer km<sup>-2</sup>")
 
-# Show only 1 density
-deer_s <- deer_s |>
-  filter(density=="14.3 deer km<sup>-2</sup>")
+# deer_s22 <- deer_s |>
+#   mutate(x=case_when(stage=="Fawn" ~ 0.2,
+#                      stage=="Yearling" ~ 0.3,
+#                      stage=="2-year-old" ~ 0.4,
+#                      stage=="3-year-old" ~ 0.5,
+#                      stage=="4-year-old" ~ 0.6,
+#                      stage=="5+ years-old" ~ 0.7)) |>
+#   mutate(x = if_else(sex=="Female", x - 0.01, x + 0.01)) |>
+#   filter(density=="22 deer km<sup>-2</sup>")
 
-
-ds_plot <- ggplot(deer_s) +
+## Plot 14 deer/km2
+ggplot() +
   coord_cartesian(ylim=c(0,1)) +
-  geom_segment(aes(x=x, y=surv.10pct, yend=surv.90pct, color=sex), linewidth=1) +
-  geom_point(aes(x=x, y=survival, shape=source, color=sex), size=3) +
-  scale_color_manual(values=c("#21918c", "#440154")) +
-  scale_shape_manual(values=c(17, 16)) +
+  geom_point(data=rates, aes(x=x, y=survival), shape=21, color="gray10", fill="gray",  size=3, alpha=0.45) +
+  geom_segment(data=deer_s, aes(x=x, y=surv.10pct, yend=surv.90pct, color=sex), linewidth=1) +
+  geom_point(data=deer_s, aes(x=x, y=survival, color=sex), size=3) +
   guides(
     color=guide_legend(position="inside", title="Sex"),
-    shape=guide_legend(position="inside", title="Source")
   ) +
+  scale_color_manual(values=c("#21918c", "#440154")) +
   scale_x_continuous(breaks=seq(0.2,0.7, by=0.1), 
                      labels=c("Fawn", "Yearling", "2 years", "3 years", "4 years", "5+ years"), 
                      expand = expansion(mult = 0.15, add = 0)) +
   ylab("Survival probability") + xlab("Stage") +
-  theme_classic() +
-  theme(panel.border=element_rect(color="black", fill=NA, linewidth=0.5),
-        legend.position.inside = c(1,0),
-        legend.justification=c(1,0),
-        legend.background = element_rect(fill=NA),
+  facet_grid(.~density) +
+  theme(panel.grid=element_blank(),
+        legend.position.inside=c(0.01, 0.99),
+        legend.justification = c(0,1),
         axis.text=element_text(size=11),
         axis.title=element_text(size=12),
+        strip.background = element_rect(fill = NA, color = NA),
+        strip.text = element_markdown(size=12),
         legend.text=element_text(size=11),
         legend.title=element_text(size=12))
+
+## Plot 22 deer/km2
+# ggplot() +
+#   coord_cartesian(ylim=c(0,1)) +
+#   geom_point(data=rates, aes(x=x, y=survival), shape=21, color="gray20", fill="gray",  size=3, alpha=0.5) +
+#   geom_segment(data=deer_s22, aes(x=x, y=surv.10pct, yend=surv.90pct, color=sex), linewidth=1) +
+#   geom_point(data=deer_s22, aes(x=x, y=survival, color=sex), size=3) +
+#   guides(
+#     color=guide_legend(position="inside", title="Sex"),
+#   ) +
+#   scale_color_manual(values=c("#21918c", "#440154")) +
+#   scale_x_continuous(breaks=seq(0.2,0.7, by=0.1), 
+#                      labels=c("Fawn", "Yearling", "2 years", "3 years", "4 years", "5+ years"), 
+#                      expand = expansion(mult = 0.15, add = 0)) +
+#   ylab("Survival probability") + xlab("Stage") +
+#   theme(panel.grid=element_blank(),
+#         legend.position.inside=c(0.01, 0.99),
+#         legend.justification = c(0,1),
+#         axis.text=element_text(size=11),
+#         axis.title=element_text(size=12),
+#         legend.text=element_text(size=11),
+#         legend.title=element_text(size=12))
+
 
 ## Fecundity
 
